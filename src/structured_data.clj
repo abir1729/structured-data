60e80795499df8ec6183cfb268fdca05317b48a3
abir1729 <abir.cse1990@gmail.com>
2014-07-03 21:41:40 +0530

    up till rectangles.

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,32 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [x2 (+ x x)]
+    (Math/pow x2 x2)))
+
+(do-a-thing 2)
+
+
 
 (defn spiff [v]
-  :-)
+  (cond
+   (< (count v) 3) nil
+   :else (+ (get v 0) (get v 2))))
+
+(spiff [1 2 3])
+
+
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
+
+(cutify [1 2])
+
+
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[first second third] v]
+    (+ first third)))
 
 (defn point [x y]
   [x y])
@@ -19,22 +35,30 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)))
+
+(width [[1 3] [4 5]])
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (== (width rectangle) (height rectangle)))
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle) (height rectangle)))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [xp yp] point]
+    (and (<= x1 xp x2) (<= y1 yp y2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[p1 p2] inner]
+    (and (contains-point? outer p1)
+         (contains-point? outer p2))))
 
 (defn title-length [book]
   :-)
